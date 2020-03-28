9c402ab7fa83dc56f9855464c37efcd35d13315a
surmon-china <surmon@foxmail.com>
2020-03-28 22:34:37 +0800

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,6 +8,6 @@ PKG_VERSION=$(jq -r '.version' package.json)
 git fetch origin v"$PKG_VERSION" || {
   type standard-version || npm i -g standard-version
   standard-version --skip.changelog -a --release-as "$PKG_VERSION"
-  git push --follow-tags origin master
   type nrm && nrm use npm && npm publish
+  git push --follow-tags origin master
 }
