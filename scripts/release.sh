d3f9307928b558c16f80f778b30039430dda849b
surmon-china <surmon@foxmail.com>
2020-03-28 22:50:11 +0800

    Release script done

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-# https://github.com/mikeal/merge-release/blob/v4.0.7/merge-release-run.js
 PKG_VERSION=$(jq -r '.version' package.json)
 
 git fetch origin v"$PKG_VERSION" || {
   type standard-version || npm i -g standard-version
   standard-version --skip.changelog -a --release-as "$PKG_VERSION"
-  type nrm && nrm use npm && npm publish
-  git push --follow-tags origin master
+  git push --follow-tags origin master && npm publish --registry https://registry.npmjs.org/
 }
